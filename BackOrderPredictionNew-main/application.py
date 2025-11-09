80da88cbf2af595f33bf8b39e6646b49fc3a5644
lsamadhan575-SK <lsamadhan575@gmail.com>
2025-11-09 12:14:04 +0530

    port changed to 8080

--- BackOrderPredictionNew-main/application.py
+++ BackOrderPredictionNew-main/application.py
@@ -61,8 +61,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True)        
-
-        
-        
-        
+    app.run(host="0.0.0.0",port=8080,debug=True)
